5677304d9558471ff6dee3ae48c2d27758c1d6a0
Gonzalo <gonziibarberis@gmail.com>
2025-05-10 17:33:08 -0300

    cambios en service worker

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
+const CACHE_NAME = "app-cache-v2"; // ⬅️ Subí este número cada vez que subas cambios
+
+const ASSETS_TO_CACHE = ["index.html", "manifest.json", "icon-192.png", "icon-512.png", "service-worker.js"];
+
+// Guardar archivos en caché
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open("app-cache").then((cache) => cache.addAll(["index.html", "manifest.json", "icon-192.png", "icon-512.png"])));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
 });
 
+// Limpiar caches viejos
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))));
+});
+
+// Atender solicitudes desde caché o red
 self.addEventListener("fetch", (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
+
+// Escuchar pedido de skipWaiting desde la app
+self.addEventListener("message", (event) => {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
